2adf4bd9694736c5b81a15b02c729df2223eb427
Kristen <kristenkwong.wy@hotmail.com>
2017-08-10 01:06:29 -0700

    added skills

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-function main() {
-	
-}
+// smooth scrolling on anchor link clicking
+
+$('a').click(function() {
+	$('html, body').animate({
+		scrollTop: $($(this).attr('href')).offset().top 
+	}, 700);
+	return false
+})
 
 $(document).ready(main)
