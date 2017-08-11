f6ee85b525352cbda69554600034f4ab7e7b91bd
Kristen <kristenkwong.wy@hotmail.com>
2017-08-10 23:03:59 -0700

    added project section

--- js/main.js
+++ js/main.js
@@ -7,4 +7,39 @@ $('a').click(function() {
 	return false
 })
 
+$(function() {
+	$("#nav").hover(function() {
+		$(this).animate({right: '0px'},
+			{queue: false, duration: 500});
+	}, function() {
+		$(this).animate({right:'-100px'},
+			{queue: false, duration: 500});
+	});
+});
+
+$(function() {
+	$("#develop").hover(function() {
+		$(this).animate({right: '10%'},
+			{queue: false, duration: 500});
+	}, function() {
+		$(this).animate({right:'50%'},
+			{queue: false, duration: 500});
+	});
+});
+
+$(function() {
+	$("#design").hover(function() {
+		$(this).animate({left: '10%'},
+			{queue: false, duration: 500});
+	}, function() {
+		$(this).animate({left:'50%'},
+			{queue: false, duration: 500});
+	});
+});
+
+
+$(document).scroll(function() {
+	$('#nav').toggle($(this).scrollTop()>600);
+});
+
 $(document).ready(main)
